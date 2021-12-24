6f69813084a7ce39fb0647d2a65094cfa7e866ec
Farnoud Taghdissi Borhani <v-ftaghd@microsoft.com>
2021-12-23 23:21:24 -0800

    Add project files.

---
+++ Move zero To End/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Move_zero_To_End
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, 1, 2, 0, 3, 0, 0, 4, 0 };
+            MoveZero(arr);
+            Console.WriteLine(string.Join(",",arr));
+        }
+
+        public static void MoveZero(int[] arr)
+        {
+            int j = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] != 0)
+                {
+                    Swap(arr, i, j);
+                    j++;
+                }
+            }
+        } 
+
+        public static void Swap(int[] arr,int i,int j)
+        {
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+    }
+}
